e0dc44314e40289367de430a14da14025f88ca69
ganeshs19 <99865044+ganeshs19@users.noreply.github.com>
2023-04-06 16:18:49 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "no professionalism at office please niggz";
+		return "no professionalism at office please niggz!";
 	}
 
 	public static void main(String[] args) {
