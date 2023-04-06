55ecd08c4b22152cda280aab6aed456118d3f49d
ganeshs19 <99865044+ganeshs19@users.noreply.github.com>
2023-04-06 18:05:08 +0530

    commitment

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hello this is a test being deployed using a pipeline.";
+		return "Hello!!!";
 	}
 
 	public static void main(String[] args) {
